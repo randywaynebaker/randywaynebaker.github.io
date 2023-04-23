8ae360f824ae1dafff190b22beff02ed8dbf13b1
randywaynebaker <60993007+randywaynebaker@users.noreply.github.com>
2023-04-23 12:29:26 -0500

    Update automation.js

--- automation.js
+++ automation.js
@@ -2,12 +2,12 @@ const sendEmailsButton = document.getElementById('send-emails');
 
 sendEmailsButton.addEventListener('click', () => {
   // Send a request through the webhook
-  fetch('https://your-webhook-url.com', {
+  fetch('https://hook.us1.make.com/yfmdv94dyc0gpctlfvn1yg2ktbh1uxtm', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ action: 'send-emails' })
+    //body: JSON.stringify({ action: 'send-emails' })
   })
   .then(response => {
     if (response.ok) {
